e11e2d3c3d236feccf32d3886c0f11bd0eecc65d
DieroA <diero.purnama@gmail.com>
2025-05-15 09:35:43 +0700

    chore: restructure project files

---
+++ src/Datasets/cifar10.py
@@ -0,0 +1,9 @@
+from tensorflow.keras.datasets import cifar10
+from sklearn.model_selection import train_test_split
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.2, random_state = 42)
+
+if __name__ == "__main__":
+    print(x_train.shape)
+    print(type(x_train))
