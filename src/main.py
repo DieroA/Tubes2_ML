9fc130faf5c14de284455624d2d61059f44f5ee7
Diero <dieroap123@gmail.com>
2025-05-27 21:50:06 +0700

    feat: CNN | bandingin forward propagation keras dg scratch

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from Models.CNN.training.configs import CONV_LAYER_VARIATIONS
 
 from Datasets.cifar10 import x_test, y_test
 
-def main():
+def compare_scratch_keras_cnn():
     # Build CNN
     cnn = CNN.from_config(CONV_LAYER_VARIATIONS["3 Layers"])
     cnn.build()
@@ -16,11 +16,22 @@ def main():
     cnn.load("ConvLayerCount/3layers_filters32-64-128_kernels3-3-3_poolmax.weights.h5")
 
     # Forward prop
-    output = cnn.forward_scratch(x_test)
+    output_scratch = cnn.forward_scratch(x_test)
+    output_keras = cnn.model.predict(x_test)
     
     # Evaluate
-    cnn.evaluate(output, y_test)
-    print(f"f1 score: {cnn.f1_score}")
+    cnn.evaluate(output_scratch, y_test)
+    score_scratch = cnn.f1_score
 
+    cnn.evaluate(output_keras, y_test)
+    score_keras = cnn.f1_score
+
+    # Evaluate 
+    print(f"From Scratch | F1-Score: {score_scratch}")
+    print(f"Keras | F1-Score: {score_keras}")
+
+def main():
+    compare_scratch_keras_cnn()
+    
 if __name__ == "__main__":
     main()
