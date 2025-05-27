15c4c7f9eaa87f13c1fb8174adae9f6440c7c07f
Diero <dieroap123@gmail.com>
2025-05-27 14:24:34 +0700

    feat: retrain with 7 epoch

--- src/main.py
+++ src/main.py
@@ -10,18 +10,21 @@ import numpy as np
 from Datasets.cifar10 import x_test, y_test
 
 def main():
+    # Build CNN
     cnn = CNN.from_config(CONV_LAYER_VARIATIONS["3 Layers"])
     cnn.build()
-    cnn.load("3layers_filters32-64-128_kernels3-3-3_poolmax.weights.h5")
 
+    # Load Weights
+    cnn.load("ConvLayerCount/3layers_filters32-64-128_kernels3-3-3_poolmax.weights.h5")
+
+    # Forward prop
     output = cnn.forward_scratch(x_test)
-    
     y_pred = np.argmax(output, axis = 1)
     print(y_pred[:30])
     
-    f1_score = cnn.evaluate(output, y_test)
-
-    print(f"f1 score: {f1_score}")
+    # Evaluate
+    cnn.evaluate(output, y_test)
+    print(f"f1 score: {cnn.f1_score}")
 
 if __name__ == "__main__":
     main()
--- src/train_CNN.py
+++ src/train_CNN.py
@@ -14,6 +14,12 @@ from Models.CNN.training.configs import (
 
 SAVE_PATH = "Models/CNN/training"
 
+def check_dirs(*paths):
+    """Checks if every directory in @*paths exist, creates an empty folder if it doesn't."""
+
+    for path in paths:
+        os.makedirs(path, exist_ok = True)
+
 def plot_loss_curves(history, title, save_path = None):
     """Plot loss curves; saves or shows plot."""
 
@@ -40,6 +46,8 @@ def train_and_evaluate_all(variation_dict, variation_name):
 
         # Build
         model = CNN.from_config(config)
+        model.weights_dir = f"{SAVE_PATH}/weights/{variation_name}"
+
         model.build()
 
         # Train & Evaluate
@@ -52,21 +60,26 @@ def train_and_evaluate_all(variation_dict, variation_name):
 
         # Log results
         print(f"{name} | Macro F1 Score: {model.f1_score:.4f}")
-        plot_loss_curves(model.history, f"{name} | Training & Validation Loss", save_path = f"{SAVE_PATH}/plots/{name}_loss_curve.png")
+        plot_loss_curves(model.history, f"{name} | Training & Validation Loss", save_path = f"{SAVE_PATH}/plots/{variation_name}/{name}_loss_curve.png")
 
 def main():
+    # Define folder structure
+    variations = ["ConvLayerCount", "FilterSize", "KernelSize", "PoolingType"]
+    plot_paths = [os.path.join(SAVE_PATH, "plots", d) for d in variations]
+
     # Ensures save directories exists
-    os.makedirs(SAVE_PATH, exist_ok = True)
-    os.makedirs(os.path.join(SAVE_PATH, "plots"), exist_ok = True)
-    os.makedirs(os.path.join(SAVE_PATH, "logs"), exist_ok = True)
+    check_dirs(SAVE_PATH,
+                os.path.join(SAVE_PATH, "plots"),
+                *plot_paths,
+                os.path.join(SAVE_PATH, "logs"))
 
     # Redirect all prints made to log file
     with open(f"{SAVE_PATH}/logs/training_log.txt", "w") as f:
         with redirect_stdout(f):
-            train_and_evaluate_all(CONV_LAYER_VARIATIONS, "Conv Layer Count")
-            train_and_evaluate_all(FILTER_VARIATIONS, "Filter Size")
-            train_and_evaluate_all(KERNEL_SIZE_VARIATIONS, "Kernel Size")
-            train_and_evaluate_all(POOLING_VARIATIONS, "Pooling Type")
+            train_and_evaluate_all(CONV_LAYER_VARIATIONS, variations[0])
+            train_and_evaluate_all(FILTER_VARIATIONS, variations[1])
+            train_and_evaluate_all(KERNEL_SIZE_VARIATIONS, variations[2])
+            train_and_evaluate_all(POOLING_VARIATIONS, variations[3])
 
 if __name__ == "__main__":
     main()
